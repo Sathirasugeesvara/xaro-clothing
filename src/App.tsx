df641bf33edd0c2be36c0db592e9c3ac1b2b632a
Sathira Sugeesvara <sathirasugeesvara@gmail.com>
2026-04-13 21:28:29 +0530

    Add React + TypeScript Vite starter template

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="container">
+      <h1>XARO Clothing 👕</h1>
+      <p>Xaro — Where style meets attitude 🐺</p>
+    </div>
+    </>
+  );
+}
+
+export default App;
